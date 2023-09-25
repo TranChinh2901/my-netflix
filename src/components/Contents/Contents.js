fee0fd989f487444e932a3d1401748ba8ee502d9
tran chinh <chinh99078@donga.edu.vn>
2023-09-25 20:04:52 +0700

    Hoàn thành phần poster với phần ảnh slider

--- src/components/Contents/Contents.js
+++ src/components/Contents/Contents.js
@@ -1,30 +1,32 @@
 import React, { useEffect } from "react";
 import MoviesRow from "./MoviesRow";
 import { useDispatch, useSelector } from "react-redux";
-import { getNetflixOriginals } from "../store/actions";
-
-const movies = [
-  "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/v1YEOdGptCyNxnc4mJSYNd4cE8E.jpg",
-  "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/ehGIDAMaYy6Eg0o8ga0oqflDjqW.jpg",
-  "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/vd8YdaH7dzeIMGTNwQinlSiA1gV.jpg",
-  "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/u5tXuFRAz2ckLGap9JKvCCK1Mh.jpg",
-  "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/cswPVyXwQ13dFHU1KFS8dpFxIyY.jpg",
-  "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/ccJpK0rqzhQeP7Mrs2uKqObFY4L.jpg",
-  "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/4lls2JCt9g1YiusX3xLVSdSZ6cO.jpg",
-  "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/lMFi3ri8h6BYtjqNwMe9y69kTwQ.jpg",
-  "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/wUvUcLTxc83k4l8psyO2pDYHHB3.jpg",
-  "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/fcXdJlbSdUEeMSJFsXKsznGwwok.jpg",
-  "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/kMI3tgxLAZbzGOVlorUBva0kriS.jpg",
-];
+import * as ACTIONS from "../store/actions";
 
 function Contents(props) {
   const dispatch = useDispatch();
-  const { NetflixOriginals } = useSelector((state) => state.infoMovies);
+  const {
+    NetflixOriginals,
+    TrendingMovies,
+    TopRatedMovies,
+    ActionMovies,
+    ComedyMovies,
+    HorrorMovies,
+    RomanceMovies,
+    Documentaries,
+  } = useSelector((state) => state.infoMovies);
 
   useEffect(() => {
-    dispatch(getNetflixOriginals());
+    dispatch(ACTIONS.getNetflixOriginals());
+    dispatch(ACTIONS.getTrendingMovies());
+    dispatch(ACTIONS.getTopRatedMovies());
+    dispatch(ACTIONS.getActionMovies());
+    dispatch(ACTIONS.getComedyMovies());
+    dispatch(ACTIONS.getHorrorMovies());
+    dispatch(ACTIONS.getRomanceMovies());
+    dispatch(ACTIONS.getDocumentaries());
   }, [dispatch]);
-  console.log(NetflixOriginals);
+
   return (
     <div>
       <MoviesRow
@@ -32,13 +34,14 @@ function Contents(props) {
         title="Netflix Originals"
         isNetflix={true}
       />
-      <MoviesRow movies={movies} title="Trending Movies" />
-      <MoviesRow movies={movies} title="Top rated Movies" />
-      <MoviesRow movies={movies} title="Action Movies" />
-      <MoviesRow movies={movies} title="Comedy Movies" />
-      <MoviesRow movies={movies} title="Horror Movies" />
-      <MoviesRow movies={movies} title="Romance Movies" />
-      <MoviesRow movies={movies} title="Documentaries " />
+      {/* Muốn thêm hàng nào thành poster thì thêm thuộc tính isNetflix={true} vào cuối */}
+      <MoviesRow movies={TrendingMovies} title="Trending Movies" />
+      <MoviesRow movies={TopRatedMovies} title="Top rated Movies" />
+      <MoviesRow movies={ActionMovies} title="Action Movies" />
+      <MoviesRow movies={ComedyMovies} title="Comedy Movies" />
+      <MoviesRow movies={HorrorMovies} title="Horror Movies" />
+      <MoviesRow movies={RomanceMovies} title="Romance Movies" />
+      <MoviesRow movies={Documentaries} title="Documentaries " />
     </div>
   );
 }
--- src/components/store/actions/index.js
+++ src/components/store/actions/index.js
@@ -4,6 +4,7 @@ import * as Types from "../types";
 const API_KEY = "74d1111ff793d2c86a4104bab872bc34";
 const BASE_URL = "https://api.themoviedb.org/3";
 
+//Netflix Originals
 export const getNetflixOriginals = () => async (dispatch) => {
   try {
     const result = await axios.get(
@@ -15,5 +16,110 @@ export const getNetflixOriginals = () => async (dispatch) => {
     });
   } catch (error) {
     console.log("Get Netflix Api error: ", error);
+  }
+};
+
+//Trending movie
+export const getTrendingMovies = () => async (dispatch) => {
+  try {
+    const result = await axios.get(
+      `${BASE_URL}/trending/all/week?api_key=${API_KEY}&language=en-us`
+    );
+    dispatch({
+      type: Types.GET_TRENDING_MOVIES,
+      payload: result.data.results,
+    });
+  } catch (error) {
+    console.log("Get Trending Api error: ", error);
+  }
+};
+
+//Top Rated Movies
+export const getTopRatedMovies = () => async (dispatch) => {
+  try {
+    const result = await axios.get(
+      `${BASE_URL}/movie/top_rated?api_key=${API_KEY}&language=en-us`
+    );
+    dispatch({
+      type: Types.GET_TOP_RATED_MOVIES,
+      payload: result.data.results,
+    });
+  } catch (error) {
+    console.log("Get Top Rated Api error: ", error);
+  }
+};
+
+//Action Movies
+export const getActionMovies = () => async (dispatch) => {
+  try {
+    const result = await axios.get(
+      `${BASE_URL}/discover/movie?api_key=${API_KEY}&with_genres=28`
+    );
+    dispatch({
+      type: Types.GET_ACTION_MOVIES,
+      payload: result.data.results,
+    });
+  } catch (error) {
+    console.log("Get Action Api error: ", error);
+  }
+};
+
+//Comedy Movies
+export const getComedyMovies = () => async (dispatch) => {
+  try {
+    const result = await axios.get(
+      `${BASE_URL}/discover/movie?api_key=${API_KEY}&with_genres=35`
+    );
+    dispatch({
+      type: Types.GET_COMEDY_MOVIES,
+      payload: result.data.results,
+    });
+  } catch (error) {
+    console.log("Get Comedy Api error: ", error);
+  }
+};
+
+//Horror Movies
+export const getHorrorMovies = () => async (dispatch) => {
+  try {
+    const result = await axios.get(
+      `${BASE_URL}/discover/movie?api_key=${API_KEY}&with_genres=27`
+    );
+    dispatch({
+      type: Types.GET_HORROR_MOVIES,
+      payload: result.data.results,
+    });
+  } catch (error) {
+    console.log("Get Horror Api error: ", error);
+  }
+};
+
+//Romance Movies
+export const getRomanceMovies = () => async (dispatch) => {
+  try {
+    const result = await axios.get(
+      `${BASE_URL}/discover/movie?api_key=${API_KEY}&with_genres=10749`
+    );
+    dispatch({
+      type: Types.GET_ROMANCE_MOVIES,
+      payload: result.data.results,
+    });
+  } catch (error) {
+    console.log("Get Romance Api error: ", error);
+  }
+};
+
+//Documentaries
+export const getDocumentaries = () => async (dispatch) => {
+  try {
+    const result = await axios.get(
+      `${BASE_URL}/discover/movie?api_key=${API_KEY}&with_genres=99`
+    );
+    dispatch({
+      type: Types.GET_DOCUMENTARIES_MOVIES,
+      payload: result.data.results,
+    });
+  } catch (error) {
+    console.log("Get Documentaries Api error: ", error);
   }
 };
